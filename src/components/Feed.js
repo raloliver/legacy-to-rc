8ba38c63865537e3494ae43d62f1678d76044073
raloliver <contato@raloliver.com.br>
2020-10-08 06:53:04 +0100

    Dissect into components

---
+++ src/components/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FeedItem from './FeedItem';
+
+function Feed(props) {
+  return (
+    <div>
+      <FeedItem />
+    </div>
+  );
+}
+
+export default Feed;
---
+++ src/components/FeedItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function FeedItem(props) {
+  return (
+    <div className="row marketing">
+        <div className="col-lg-6">
+            <h4>test test baby</h4>
+            <p>As new modules are created, they will automatically be included in the QUnit test suite - just add your assertions to tests/test.js and "grunt test" boyyyyyyyy!</p>
+        </div>
+    </div>
+  );
+}
+
+export default FeedItem;
